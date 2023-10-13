02468496d22ef05fdc3d64efdbee72a0aa0e0034
yayoimizuha <poko.peko.tomo@icloud.com>
2023-10-14 01:19:16 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
-use std::io::Read;
-use futures::{StreamExt, stream, future};
-use reqwest::{Client, Response};
-use tokio::io::AsyncReadExt;
+use futures::future;
+use reqwest::Client;
 use tokio::task;
+use regex::Regex;
 
-const NAMES: &[&str] = &["tsubaki-factory", "angerme-ss-shin"];
+// const NAMES: &[&str] = &["angerme-ss-shin", "angerme-amerika", "angerme-new", "juicejuice-official",
+//     "tsubaki-factory", "morningmusume-9ki", "morningmusume-10ki", "mm-12ki", "morningm-13ki",
+//     "morningmusume15ki", "morningmusume16ki", "beyooooonds-rfro", "beyooooonds-chicatetsu",
+//     "beyooooonds", "ocha-norma", "countrygirls", "risa-ogata", "kumai-yurina-blog",
+//     "sudou-maasa-blog", "sugaya-risako-blog", "miyamotokarin-official", "kobushi-factory",
+//     "sayumimichishige-blog", "kudo--haruka", "airisuzuki-officialblog", "angerme-ayakawada",
+//     "miyazaki-yuka-blog", "tsugunaga-momoko-blog", "tokunaga-chinami-blog", "c-ute-official",
+//     "tanakareina-blog"];
+const NAMES: &[&str] = &["angerme-ss-shin", "angerme-amerika"];
 
-// implement get_text function
-async fn get_text(client: Client, name: &str) -> String {
-    // get response from the url
-    let mut response = client.get(&format!("https://helloproject.fandom.com/wiki/{}", name))
+
+async fn get_page_count(client: Client, name: &str, order: usize, page_count: Regex) -> String {
+    // println!("start: {order}_{name}");
+    let response = client.get(&format!("https://ameblo.jp/{name}/entrylist.html"))
         .send().await.unwrap();
-    // read response body as text
-    let mut text = String::new();
-    response.read_to_string(&mut text).unwrap();
-    // return text
+    let text = response.text().await.unwrap();
+    println!("end: {order}_{name}");
+    let mut json = page_count.captures(&text).unwrap().get(0).unwrap().as_str().to_string();
+    json.pop();
+    let _json = json.replacen("<script>window.INIT_DATA=", "", 1);
+    let json_parse: serde_json::Value = serde_json::from_str(&_json).unwrap();
+    println!("page count: {:?}", json_parse["entryState"]["blogPageMap"]);
+
     text
 }
 
-// main function
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
     let mut tasks = Vec::new();
-    // get all NAMES contents concurrently using reqwest as text
-    for name in NAMES {
-        let task = task::spawn(get_text(client.clone(), name));
+    let page_count: Regex = Regex::new(r"<script>window.INIT_DATA=(.*?)};").unwrap();
+
+    for i in 0..NAMES.len() {
+        let task = task::spawn(get_page_count(client.clone(),
+                                              NAMES[i], i, page_count.clone()));
         tasks.push(task);
     }
-    // get all tasks result concurrently
-    let results = future::join_all(tasks).await;
-    // print out all results
-    for result in results {
-        println!("{}", result.unwrap());
-    }
+    let _results = future::join_all(tasks).await;
 }
