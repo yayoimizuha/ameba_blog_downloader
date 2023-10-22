e9cd3df0f45a742abdd61bb86793939bd1c4b30c
Tomokazu Katayama <poko.peko.tomo@icloud.com>
2023-10-23 06:24:51 +0900

    update

--- src/bin/onnx_test.rs
+++ src/bin/onnx_test.rs
@@ -3,11 +3,12 @@ use image::{GenericImageView};
 use ort::{Environment, GraphOptimizationLevel, SessionBuilder, Value};
 use ort::ExecutionProvider;
 use ndarray::Array;
-use ort::execution_providers::CUDAExecutionProviderOptions;
+use ort::execution_providers::{CPUExecutionProviderOptions, CUDAExecutionProviderOptions};
+use ort::tensor::OrtOwnedTensor;
 
 // 参考: https://github.com/AndreyGermanov/yolov8_onnx_rust/blob/5b28d2550d715f7dbed8ce31b5fdb8e000fa77f6/src/main.rs
 
-fn main() {
+fn main() -> () {
     let image_path = r"manaka_test.jpg";
     let onnx_path = r"retinaface.onnx";
     match fs::metadata(onnx_path) {
@@ -22,13 +23,14 @@ fn main() {
     tracing_subscriber::fmt::init();
     let environment = Environment::builder()
         .with_name("RetinaFace")
+        //.with_execution_providers([ExecutionProvider::CUDA(CUDAExecutionProviderOptions::default())])
         .with_execution_providers([ExecutionProvider::CUDA(CUDAExecutionProviderOptions::default())])
         .build().unwrap()
         .into_arc();
 
     let session = SessionBuilder::new(&environment).unwrap()
-        .with_optimization_level(GraphOptimizationLevel::Level1).unwrap()
-        .with_intra_threads(1).unwrap()
+        // .with_optimization_level(GraphOptimizationLevel::Level1).unwrap()
+        // .with_intra_threads(1).unwrap()
         .with_model_from_file(onnx_path).unwrap();
 
     let image = image::open(image_path).unwrap();
@@ -39,9 +41,9 @@ fn main() {
         let x = pixel.0 as usize;
         let y = pixel.1 as usize;
         let [r, g, b, _] = pixel.2.0;
-        image_arr[[0, 0, y, x]] = (r as f32) / 1.0;
-        image_arr[[0, 1, y, x]] = (g as f32) / 1.0;
-        image_arr[[0, 2, y, x]] = (b as f32) / 1.0;
+        image_arr[[0, 0, y, x]] = (r as f32) / 255.0;
+        image_arr[[0, 1, y, x]] = (g as f32) / 255.0;
+        image_arr[[0, 2, y, x]] = (b as f32) / 255.0;
     }
     let image_layout = image_arr.as_standard_layout();
 
@@ -51,9 +53,11 @@ fn main() {
     println!("{:?}", onnx_input);
     println!("{}", onnx_input.get(0).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
     let model_res = session.run(onnx_input).unwrap();
-    println!("{:?}", model_res.get(0).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
-    println!("{:?}", model_res.get(1).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
-    println!("{:?}", model_res.get(2).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
+    let out_1: OrtOwnedTensor<f32, _> = model_res.get(0).unwrap().try_extract().unwrap();
+    println!("{:?}", out_1);
+    // println!("{:?}", model_res.get(0).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
+    // println!("{:?}", model_res.get(1).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
+    // println!("{:?}", model_res.get(2).unwrap().try_extract::<f32>().unwrap().view().clone().into_owned());
 
-    return;
+    return ();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,75 @@
-use std::ops::Deref;
 use futures::future;
 use futures::stream::iter;
-use reqwest::Client;
-use tokio::{task, time};
 use regex::Regex;
+use reqwest::Client;
+use std::ops::Deref;
+use tokio::{task, time};
 
-const NAMES: &[&str] = &["angerme-ss-shin", "angerme-amerika", "angerme-new", "juicejuice-official",
-    "tsubaki-factory", "morningmusume-9ki", "morningmusume-10ki", "mm-12ki", "morningm-13ki",
-    "morningmusume15ki", "morningmusume16ki", "beyooooonds-rfro", "beyooooonds-chicatetsu",
-    "beyooooonds", "ocha-norma", "countrygirls", "risa-ogata", "kumai-yurina-blog",
-    "sudou-maasa-blog", "sugaya-risako-blog", "miyamotokarin-official", "kobushi-factory",
-    "sayumimichishige-blog", "kudo--haruka", "airisuzuki-officialblog", "angerme-ayakawada",
-    "miyazaki-yuka-blog", "tsugunaga-momoko-blog", "tokunaga-chinami-blog", "c-ute-official",
-    "tanakareina-blog"];
+const NAMES: &[&str] = &[
+    "angerme-ss-shin",
+    "angerme-amerika",
+    "angerme-new",
+    "juicejuice-official",
+    "tsubaki-factory",
+    "morningmusume-9ki",
+    "morningmusume-10ki",
+    "mm-12ki",
+    "morningm-13ki",
+    "morningmusume15ki",
+    "morningmusume16ki",
+    "beyooooonds-rfro",
+    "beyooooonds-chicatetsu",
+    "beyooooonds",
+    "ocha-norma",
+    "countrygirls",
+    "risa-ogata",
+    "kumai-yurina-blog",
+    "sudou-maasa-blog",
+    "sugaya-risako-blog",
+    "miyamotokarin-official",
+    "kobushi-factory",
+    "sayumimichishige-blog",
+    "kudo--haruka",
+    "airisuzuki-officialblog",
+    "angerme-ayakawada",
+    "miyazaki-yuka-blog",
+    "tsugunaga-momoko-blog",
+    "tokunaga-chinami-blog",
+    "c-ute-official",
+    "tanakareina-blog",
+];
 
 // const NAMES: &[&str] = &["angerme-ss-shin", "angerme-amerika"];
-
 
 async fn get_page_count(client: Client, name: &str, _order: usize, page_count: Regex) -> u64 {
     let mut page_nums: Option<u64> = None;
 
-    let async_wait = |t: u64| async move {
-        time::sleep(time::Duration::from_millis(t)).await
-    };
+    let async_wait = |t: u64| async move { time::sleep(time::Duration::from_millis(t)).await };
     while page_nums.is_none() {
-        match client.get(&format!("https://ameblo.jp/{name}/entrylist.html"))
-            .send().await {
+        match client
+            .get(&format!("https://ameblo.jp/{name}/entrylist.html"))
+            .send()
+            .await
+        {
             Ok(response) => {
                 let text = response.text().await.unwrap();
-                match page_count.captures(&*text).unwrap().get(0).
-                    ok_or(&text) {
+                match page_count.captures(&*text).unwrap().get(0).ok_or(&text) {
                     Ok(matched_text) => {
                         let mut json_str = matched_text.as_str().to_string();
                         json_str.pop();
                         json_str = json_str.replacen("<script>window.INIT_DATA=", "", 1);
                         match serde_json::from_str::<serde_json::Value>(&json_str) {
                             Ok(json) => {
-                                let _ = json["entryState"]["blogPageMap"].as_object().unwrap().iter()
+                                let _ = json["entryState"]["blogPageMap"]
+                                    .as_object()
+                                    .unwrap()
+                                    .iter()
                                     .map(|x| {
-                                        page_nums = Option::from(x.1["paging"]["max_page"].as_u64().unwrap());
-                                    }).collect::<Vec<_>>();
+                                        page_nums = Option::from(
+                                            x.1["paging"]["max_page"].as_u64().unwrap(),
+                                        );
+                                    })
+                                    .collect::<Vec<_>>();
                             }
                             Err(err) => {
                                 eprintln!("Failed to parse json string: {}", err);
@@ -64,7 +94,6 @@ async fn get_page_count(client: Client, name: &str, _order: usize, page_count: R
     //page_count
 }
 
-
 #[tokio::main]
 async fn main() {
     let client = Client::new();
@@ -72,13 +101,20 @@ async fn main() {
     let page_count: Regex = Regex::new(r"<script>window.INIT_DATA=(.*?)};").unwrap();
 
     for i in 0..NAMES.len() {
-        let task = task::spawn(get_page_count(client.clone(),
-                                              NAMES[i], i, page_count.clone()));
+        let task = task::spawn(get_page_count(
+            client.clone(),
+            NAMES[i],
+            i,
+            page_count.clone(),
+        ));
         tasks.push(task);
     }
 
-    let list_page_count: Vec<_> = future::join_all(tasks).await.iter()
-        .map(|x| { x.as_ref().unwrap().clone() }).collect();
+    let list_page_count: Vec<_> = future::join_all(tasks)
+        .await
+        .iter()
+        .map(|x| x.as_ref().unwrap().clone())
+        .collect();
     for &i in &list_page_count {
         println!("{}", i);
     }
