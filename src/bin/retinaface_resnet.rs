0aa636251d72a69376fc8400a3f2eae43c7dcd4e
yayoimizuha <poko.peko.tomo@icloud.com>
2024-02-22 22:20:12 +0900

    add RetinaFace (MobileNet0.25 ResNet)

--- src/bin/retinaface_resnet.rs
+++ src/bin/retinaface_resnet.rs
@@ -1,34 +1,34 @@
 use std::{f32, fs};
 use std::env::args;
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Div, Mul};
+use std::time::Instant;
 use image::DynamicImage;
-use image::imageops::FilterType;
-use itertools::{enumerate, iproduct, Itertools};
-use ndarray::{arr1, arr2, Array, array, Array2, Array4, Axis, concatenate, Ix1, Ix2, s, stack};
-use ort::{CPUExecutionProvider, CUDAExecutionProvider, DirectMLExecutionProvider, ExecutionProvider, ExecutionProviderDispatch, GraphOptimizationLevel, inputs, Session, TensorRTExecutionProvider, Value};
+use itertools::{enumerate, iproduct};
+use ndarray::{arr2, Array, array, Array2, Array4, Axis, concatenate, Ix1, Ix2, s};
+use ort::{ArrayExtensions, CPUExecutionProvider, CUDAExecutionProvider, DirectMLExecutionProvider, ExecutionProvider, GraphOptimizationLevel, inputs, Session, TensorRTExecutionProvider, Value};
 use powerboxesrs::nms::nms;
 use serde::{Deserialize, Serialize};
 
-const ONNX_PATH: &str = r#"C:\Users\tomokazu\CLionProjects\ameba_blog_downloader\src\bin\mobilenet_retinaface.onnx"#;
+const ONNX_PATH: &str = r#"C:\Users\tomokazu\RustroverProjects\ameba_blog_downloader\src\bin\resnet_retinaface.onnx"#;
 
 
-fn transform(image: DynamicImage, max_size: usize) -> Array4<f32> {
-    let resized_image = if max_size < image.width() as usize || max_size < image.height() as usize {
-        // image.resize(max_size as u32, max_size as u32, FilterType::Triangle)
-        image
-    } else {
-        // image.resize(max_size as u32, max_size as u32, FilterType::Triangle)
-        image
-    };
-    // resized_image.save("prepare.jpg").unwrap();
-    let mut output_image = Array4::zeros((1usize, 3usize, resized_image.height() as usize, resized_image.width() as usize));
+fn transform(image: DynamicImage, _max_size: usize) -> Array4<f32> {
+    // let resized_image = if max_size < image.width() as usize || max_size < image.height() as usize {
+    //     // image.resize(max_size as u32, max_size as u32, FilterType::Triangle)
+    //     image
+    // } else {
+    //     // image.resize(max_size as u32, max_size as u32, FilterType::Triangle)
+    //     image
+    // };
+    // // resized_image.save("prepare.jpg").unwrap();
+    let mut output_image = Array4::zeros((1usize, 3usize, image.height() as usize, image.width() as usize));
     output_image.fill(0.);
 
-    for (x, y, pixel) in resized_image.into_rgb32f().enumerate_pixels() {
+    for (x, y, pixel) in image.into_rgb32f().enumerate_pixels() {
         let [r, g, b] = pixel.0; // Normalize
-        output_image[[0usize, 0, y as usize, x as usize]] = r;//(r as i16 - 104) as f32;
-        output_image[[0usize, 1, y as usize, x as usize]] = g;//(g as i16 - 117) as f32;
-        output_image[[0usize, 2, y as usize, x as usize]] = b;//(b as i16 - 123) as f32;
+        output_image[[0usize, 0, y as usize, x as usize]] = (r - 0.485) / 0.229;
+        output_image[[0usize, 1, y as usize, x as usize]] = (g - 0.456) / 0.224;
+        output_image[[0usize, 2, y as usize, x as usize]] = (b - 0.406) / 0.225;
     }
     // for (x, y, pixel) in resized_image.into_rgb8().enumerate_pixels() {
     //     let [r, g, b] = pixel.0; // Normalize
@@ -45,22 +45,21 @@ fn transform(image: DynamicImage, max_size: usize) -> Array4<f32> {
     output_image
 }
 
-
-fn prior_box(min_sizes: Vec<Vec<usize>>, steps: Vec<usize>, clip: bool, image_size: [usize; 2]) -> Array2<f32> {
+fn prior_box(min_sizes: Vec<Vec<usize>>, steps: Vec<usize>, clip: bool, image_size: [usize; 2]) -> (Array2<f32>, usize) {
     let feature_maps = steps.iter().map(|&step| {
         [f32::ceil(image_size[0] as f32 / step as f32) as i32,
             f32::ceil(image_size[1] as f32 / step as f32) as i32]
     }).collect::<Vec<_>>();
-    println!("{:?}", feature_maps);
+    // println!("{:?}", feature_maps);
     let mut anchors: Vec<[f32; 4]> = vec![];
     for (k, f) in enumerate(feature_maps) {
         for (i, j) in iproduct!(0..f[0],0..f[1]) {
             let t_min_sizes = &min_sizes[k];
             for &min_size in t_min_sizes {
-                let s_kx = min_size as f32 / image_size[1] as f32;
-                let s_ky = min_size as f32 / image_size[0] as f32;
-                let dense_cx = [j as f32 + 0.5].iter().map(|x| x * steps[k] as f32 / image_size[1] as f32).collect::<Vec<_>>();
-                let dense_cy = [i as f32 + 0.5].iter().map(|y| y * steps[k] as f32 / image_size[0] as f32).collect::<Vec<_>>();
+                let s_kx = min_size as f32 / image_size[0] as f32;
+                let s_ky = min_size as f32 / image_size[1] as f32;
+                let dense_cx = [j as f32 + 0.5].iter().map(|x| x * steps[k] as f32 / image_size[0] as f32).collect::<Vec<_>>();
+                let dense_cy = [i as f32 + 0.5].iter().map(|y| y * steps[k] as f32 / image_size[1] as f32).collect::<Vec<_>>();
                 for (cy, cx) in iproduct!(dense_cy,dense_cx) {
                     anchors.push([cx, cy, s_kx, s_ky]);
                 }
@@ -71,7 +70,7 @@ fn prior_box(min_sizes: Vec<Vec<usize>>, steps: Vec<usize>, clip: bool, image_si
     if clip {
         output = output.mapv(|x| f32::min(f32::max(x, 0.0), 1.0));
     }
-    output
+    (output, anchors.len())
 }
 
 fn decode(loc: Array<f32, Ix2>, priors: Array<f32, Ix2>, variances: [f32; 2]) -> Array<f32, Ix2> {
@@ -89,25 +88,6 @@ fn decode(loc: Array<f32, Ix2>, priors: Array<f32, Ix2>, variances: [f32; 2]) ->
     boxes
 }
 
-fn nms_impl(boxes: Array<f32, Ix2>, scores: Array<f32, Ix1>, threshold: f32) {
-    let x1 = boxes.slice(s![..,1]).to_owned();
-    let y1 = boxes.slice(s![..,2]).to_owned();
-    let x2 = boxes.slice(s![..,3]).to_owned();
-    let y2 = boxes.slice(s![..,4]).to_owned();
-    // let scores = scores.as_slice().unwrap();
-
-    let areas = (x2 - x1).add(1.) * (y2 - y1).add(1.);
-    let order = scores.iter().enumerate().sorted_by_key(|x| x.1).map(|x| x.0).collect::<Vec<_>>();
-
-    let mut keep = vec![];
-
-    while order.len() > 0 {
-        let i = order.get(0).unwrap();
-        keep.push(i);
-
-        let xx1 = x1.iter()
-    }
-}
 
 fn decode_landmark(pre: Array<f32, Ix2>, priors: Array<f32, Ix2>, variances: [f32; 2]) -> Array<f32, Ix2> {
     return concatenate(Axis(1),
@@ -149,8 +129,9 @@ pub fn infer(image_bytes: Vec<u8>) -> Result<Vec<FoundFace>, String> {
         .with_model_from_file(ONNX_PATH).unwrap();
 
 
-    let confidence_threshold = 0.6;
-    let nms_threshold = 0.7;
+    let confidence_threshold = 0.7;
+    let nms_threshold = 0.4;
+    let variance = [0.1, 0.2];
 
     let image;
     match image::load_from_memory(image_bytes.as_slice()) {
@@ -180,46 +161,47 @@ pub fn infer(image_bytes: Vec<u8>) -> Result<Vec<FoundFace>, String> {
 
     let binding = raw_image.to_owned();
     let input_shape = binding.shape();
-    // println!("{}", raw_image);
     let onnx_input = inputs!["input"=>raw_image.view()].unwrap();
-    let model_res = session.run(onnx_input).unwrap();
+    let transformed_size = array![input_shape[2], input_shape[3]].to_owned();
 
-    let transformed_size = array![input_shape[3], input_shape[2]].to_owned();
+    // println!("{}", raw_image);
+
+    let now = Instant::now();
+    let model_res = session.run(onnx_input).unwrap();
+    println!("ONNX Inference time: {:?}", now.elapsed());
+
+    let extract = |tensor: &Value| tensor.extract_tensor::<f32>().unwrap().view().to_owned();
+    let [ confidence, loc, landmark] = ["confidence", "bbox", "landmark"].map(|label| extract(model_res.get(label).unwrap()));
+
     let scale_landmarks = concatenate(Axis(0), &*vec![transformed_size.view(); 5]).unwrap().mapv(|x| x as f32);
     let scale_bboxes = concatenate(Axis(0), &*vec![transformed_size.view(); 2]).unwrap().mapv(|x| x as f32);
-    let variance = [0.1, 0.2];
-    let prior_box = prior_box(
+
+    let (prior_box, _onnx_output_width) = prior_box(
         vec![vec![16, 32], vec![64, 128], vec![256, 512]],
         [8, 16, 32].into(),
         false,
         [input_shape[2], input_shape[3]],
     );
-    // println!("{}", prior_box);
-    // const ONNX_OUTPUT_WIDTH: usize = 16800;
-    let extract = |tensor: &Value, _: usize| tensor.extract_tensor::<f32>().unwrap().view().to_owned();
-    let [ confidence, loc, landmark]
-        = [("confidence", 2), ("bbox", 4), ("landmark", 10)].map(|(label, width)| extract(model_res.get(label).unwrap(), width));
-
-    // let confidence = confidence.softmax(Axis(2));
-
-    // println!("{}", loc);
 
 
-    // println!("\n");
+    let confidence = confidence.softmax(Axis(2));
+
     let mut boxes = decode(loc.slice(s![0,..,..]).to_owned(), prior_box.clone(), variance);
     boxes = boxes * scale_bboxes;
-    let scores = confidence.slice(s![0,..,1]).to_owned() as Array<f32, Ix1>;
+
+    let mut scores = confidence.slice(s![0,..,1]).to_owned() as Array<f32, Ix1>;
+
     let mut landmarks = decode_landmark(landmark.slice(s![0,..,..]).to_owned(), prior_box.clone(), variance);
     landmarks = landmarks * scale_landmarks;
 
-    println!("{}", landmarks);
-    println!("{:?}", landmarks.dim());
 
-    let det_score = scores.clone().insert_axis(Axis(1));
-    let determ = concatenate(Axis(1), &[boxes.view(), det_score.view()]).unwrap();
-    println!("{:?}", determ.dim());
+    let valid_index = scores.iter().enumerate().filter(|(_, val)| val > &&confidence_threshold).map(|(order, _)| order).collect::<Vec<_>>();
+    boxes = boxes.select(Axis(0), &*valid_index);
+    landmarks = landmarks.select(Axis(0), &*valid_index);
+    scores = scores.select(Axis(0), &*valid_index);
 
-    let keep = nms(&boxes, &scores.mapv(|x| x as f64), nms_threshold, confidence_threshold).into_iter().collect::<Vec<_>>();
+
+    let keep = nms(&boxes.to_owned(), &scores.mapv(|x| x as f64).to_owned(), nms_threshold, confidence_threshold as f64);
 
 
     let mut faces = vec![];
@@ -229,11 +211,7 @@ pub fn infer(image_bytes: Vec<u8>) -> Result<Vec<FoundFace>, String> {
             score: *scores.get(index).unwrap(),
             landmarks: <[[f32; 2]; 5]>::try_from(landmarks.slice(s![index,..]).to_vec().chunks_exact(2).map(|x| { <[f32; 2]>::try_from(x).unwrap() }).collect::<Vec<_>>()).unwrap(),
         });
-        // print!("{}\t", boxes.slice(s![index,..]));
-        // print!("{}\t", scores.slice(s![index]));
-        // println!("{}", landmarks.slice(s![index,..]));
     }
-    // println!("{:?}", faces);
 
     Ok(faces)
 }
@@ -244,10 +222,12 @@ fn main() {
     let image = fs::read(image_path).unwrap();
 
     #[cfg(not(target_arch = "wasm32"))]{
+        let now = Instant::now();
         let infer_res = infer(image).unwrap();
+        println!("{:?}", now.elapsed());
         println!("{}", infer_res.len());
         for face in infer_res {
-            // println!("{:?}", face);
+            println!("{:?}", face);
         }
     };
     #[cfg(target_arch = "wasm32")]{
