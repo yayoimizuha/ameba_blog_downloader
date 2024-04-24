6a2de14b77ba25fcea86ed8f41f023ef8edf9f93
yayoimizuha <poko.peko.tomo@icloud.com>
2024-04-24 10:33:21 +0900

    update

--- src/bin/detect_face_test.rs
+++ src/bin/detect_face_test.rs
@@ -1,16 +1,34 @@
-use std::path::Path;
-use std::fs;
+use std::env;
+use std::path::PathBuf;
+use std::sync::Arc;
+use futures::future::join_all;
+use once_cell::sync::Lazy;
+use tokio::fs;
+use tokio::sync::Semaphore;
 use zune_jpeg::JpegDecoder;
+use zune_jpeg::zune_core::bytestream::ZCursor;
 
-const DATA_PATH: &str = r#"D:\helloproject-ai-data"#;
+const DATA_PATH: Lazy<PathBuf> = Lazy::new(|| {
+    match env::var("DATA_PATH") {
+        Ok(str) => { PathBuf::from(str) }
+        Err(_) => { PathBuf::from(r#"D:\helloproject-ai-data"#) }
+    }
+});
+static SEMAPHORE: Lazy<Arc<Semaphore>> = Lazy::new(|| Arc::new(Semaphore::new(200)));
 
+async fn decode(path: PathBuf) -> Vec<u8> {
+    let _permit = SEMAPHORE.acquire().await.unwrap();
+    let file_content = fs::read(&path).await.unwrap();
+    let mut decoder = JpegDecoder::new(ZCursor::new(&file_content));
+    let dec_res = decoder.decode().unwrap();
+    println!("{:?}", path);
+    dec_res
+}
 
-fn main() {
-    let image_dir = Path::new(DATA_PATH).join("blog_images").read_dir().unwrap().map(|dir|dir.unwrap().path().read_dir().unwrap().into_iter().map(|file|file.unwrap().path())).flatten().collect::<Vec<_>>();
+#[tokio::main]
+async fn main() {
+    let image_dir = DATA_PATH.join("blog_images").read_dir().unwrap().map(|dir| dir.unwrap().path().read_dir().unwrap().into_iter().map(|file| file.unwrap().path())).flatten().collect::<Vec<_>>();
     // println!("{:?}", image_dir);
-    let _ = image_dir.iter().map(|file| {
-        let file_content = fs::read(file).unwrap();
-        let mut decoder =JpegDecoder::new(&*file_content);
-        decoder.decode().unwrap();
-    }).collect::<Vec<_>>();
+    let ls = image_dir.into_iter().map(|file| tokio::spawn(decode(file))).collect::<Vec<_>>();
+    join_all(ls).await.into_iter();
 }
