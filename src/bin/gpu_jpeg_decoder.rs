e0c40c31a03dadc36649b3ad00b8559448186d9b
yayoimizuha <poko.peko.tomo@icloud.com>
2024-04-27 01:28:51 +0900

    update

--- src/bin/gpu_jpeg_decoder.rs
+++ src/bin/gpu_jpeg_decoder.rs
@@ -1,3 +1,10 @@
+use std::fs;
+use std::fs::File;
 use wgpu::{Backend};
+use zune_jpeg::JpegDecoder;
+use zune_jpeg::zune_core::bytestream::ZCursor;
 
-fn main() {}
+fn main() {
+    let file_bin = fs::read(r#"C:\Users\tomokazu\friends-4385686.jpg"#).unwrap();
+    let jpeg_data = JpegDecoder::new(ZCursor::new(file_bin)).decode().unwrap();
+}
