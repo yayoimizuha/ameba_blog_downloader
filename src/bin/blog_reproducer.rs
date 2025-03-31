6131c50fb81e5a46733d43089b6ad7849cfbb690
Tomokazu Katayama <poko.peko.tomo@icloud.com>
2025-03-31 17:57:59 +0900

    update

--- src/bin/blog_reproducer.rs
+++ src/bin/blog_reproducer.rs
@@ -1,9 +1,12 @@
 use std::env::args;
+use std::fmt::format;
 use std::path::PathBuf;
 use futures::future::join_all;
+use ort::tensor::Utf8Data;
 use sqlx::SqlitePool;
 use sqlx::sqlite::SqliteConnectOptions;
-use tokio::fs::create_dir;
+use tokio::fs::{create_dir, File};
+use tokio::io::AsyncWriteExt;
 use ameba_blog_downloader::data_dir;
 
 #[tokio::main]
@@ -22,11 +25,17 @@ async fn main() {
         }
     }).collect::<Vec<_>>()).await;
     let _ = join_all(blog_keys.iter().map(async |key| {
-        join_all([
+        let themes = [
             vec!["全員".to_owned()],
             sqlx::query_as("SELECT DISTINCT theme FROM blog WHERE blog_key = ?;").bind(key).fetch_all(&connection).await.unwrap().iter().map(|(x, ): &(String,)| { x.clone() }).collect::<Vec<_>>()
-        ].concat().iter().map(async |theme| {
+        ].concat();
+        join_all(themes.iter().map(async |theme| {
+            let mut index_path = match theme.as_str() {
+                "全員" => { File::create(dest.join(key).join(theme)) }
+                name => { File::create(dest.join(key).join(name)) }
+            }.await.unwrap();
             
+            index_path.write("".as_utf8_bytes()).await.unwrap();
         })).await;
     }).collect::<Vec<_>>()).await;
 }
